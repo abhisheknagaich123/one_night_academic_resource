22578221c2ee4c7a10c95bdcf31b2e6d1fa79836
Abhishek Nagaich <abhishek.nagaich_cs21@gla.ac.in>
2024-03-01 10:33:40 +0530

    commit

---
+++ navbar.php
@@ -0,0 +1,50 @@
+
+  
+    <div class="container-fluid">
+        <div class="row border-top px-xl-5">
+            <div class="col-lg-3 d-none d-lg-block">
+                <a class="d-flex align-items-center justify-content-between bg-secondary w-100 text-decoration-none"
+                    data-toggle="collapse" href="#navbar-vertical" style="height: 67px; padding: 0 30px;">
+                    <h5 class="text-primary m-0"><i class="fa fa-book-open mr-2"></i>Subjects</h5>
+                    <i class="fa fa-angle-down text-primary"></i>
+                </a>
+                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 border border-top-0 border-bottom-0 bg-light"
+                    id="navbar-vertical" style="width: calc(100% - 30px); z-index: 9;">
+                    <div class="navbar-nav w-100">
+                        
+                        <a href="" class="nav-item nav-link">DBMS</a>
+                        <a href="" class="nav-item nav-link">Operating System</a>
+                        <a href="" class="nav-item nav-link">Java</a>
+                        <a href="" class="nav-item nav-link">Design and Analysis of algorithms</a>
+                    </div>
+                </nav>
+            </div>
+            <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
+                    <a href="" class="text-decoration-none d-block d-lg-none">
+                        <h1 class="m-0"><span class="text-primary">E</span>COURSES</h1>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav py-0">
+                            <a href="#" class="nav-item nav-link active">Home</a>
+                            <a href="#" class="nav-item nav-link">Teachers</a>
+                            <a href="#" class="nav-item nav-link">About</a>
+                            <a href="#" class="nav-item nav-link">Courses</a>
+                            <div class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Blog</a>
+                                <div class="dropdown-menu rounded-0 m-0">
+                                    <a href="#" class="dropdown-item">Blog List</a>
+                                    <a href="#" class="dropdown-item">Blog Detail</a>
+                                </div>
+                            </div>
+                            <a href="#" class="nav-item nav-link">Contact</a>
+                        </div>
+                        <a class="btn btn-primary py-2 px-4 ml-auto d-none d-lg-block" href="#">Join Now</a>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
---
+++ test.php
@@ -0,0 +1,99 @@
+<?php 
+ // echo $_SERVER['PHP_SELF']."<br>";
+//  echo $_SERVER['SERVER_NAME']."<br>";
+//  echo $_SERVER['HTTP_HOST']."<br>";
+// // echo $_SERVER['HTTP_REFERER'];
+// // echo $_SERVER['HTTP_USER_AGENT'];
+// // echo $_SERVER['SCRIPT_NAME'];
+///  echo ;
+// function fname(){
+// 	echo "string";
+// }
+// function SMALL(){
+// 	echo"RAM RAM JI";
+// }
+// FNAME();
+// small();
+//echo "Hello", " ", "World";  // Outputs: Hello World
+
+// With parentheses (also valid)
+//print("Hello" "World");
+// $result = @file_get_contents('nonexistent_file.txt');
+
+// // If the file does not exist, no error message will be displayed
+// // and $result will be set to false.
+// if ($result === false) {
+//     echo "Error: Unable to read the file.";
+// }
+
+// $code = '<?php echo "Hello, World!";/
+
+
+
+
+// echo"ram ji \"and\" shayma ji";
+
+// class Fruit {
+//   // Properties
+//   public $name;
+//   public $color;
+
+//   // Methods
+//   function set_name($name) {
+//     $this->name = $name;
+//   }
+//   function get_name() {
+//     return $this->name;
+//   }
+//   function set_color($color) {
+//     $this->color = $color;
+//   }
+//   function get_color() {
+//     return $this->color;
+//   }
+// }
+
+// $apple = new Fruit();
+// $apple->set_name('Apple');
+// $apple->set_color('Red');
+// echo "Name: " . $apple->get_name();
+// echo "<br>";
+// echo "Color: " . $apple->get_color();
+// class fruit{
+// public $name;
+// public $price;
+
+// 	function set_name($name){
+// 		$this->name=$name;
+
+// 	}
+// 	function set_price($price){
+// 		$this->price;
+
+// 	}
+// 	function get_data(){
+// 		return $this->name;
+
+// 	}
+// }
+// $apple=new fruit();
+// $apple->set_name('apple');
+
+//  echo "Color: " . $apple->get_data();
+// class Goodbye {
+//   const LEAVING_MESSAGE = "Thank you for visiting W3Schools.com!";
+// }
+//  echo Goodbye::LEAVING_MESSAGE;
+
+
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname="antino";
+// //$conn=new mysqli($servername,$username,$password,$dbname);
+// $conn=mysqli_connect("localhost","root","","antino");
+// if (!$conn) {
+//   die("Connection failed: " . $conn->connect_error);
+// }
+// echo "Connected successfully";
+//  ?>
